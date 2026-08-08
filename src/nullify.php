d989a034fbcef9d877cc6ae971243c98f7d8c1bb
Vic Cherubini <vcherubini@gmail.com>
2026-08-08 00:37:45 -0500

    wip

--- src/nullify.php
+++ src/nullify.php
@@ -1,7 +1,30 @@
 <?php
 
-if (!function_exists('nullify')) {
-    function nullify(mixed $value, bool $trim = true): ?string
+use Symfony\Component\String\Exception\ExceptionInterface;
+
+use function Symfony\Component\String\u;
+
+if (!\function_exists(nullify::class)) {
+    function nullify(string|Stringable|null $string, bool $trim = true): ?string
     {
+        if (null === $string) {
+            return $string;
+        }
+
+        if ($string instanceof Stringable) {
+            $string = $string->__toString();
+        }
+
+        try {
+            $us = u($string);
+        } catch (ExceptionInterface) {
+            return null;
+        }
+
+        if (true === $trim) {
+            $us = $us->trim();
+        }
+
+        return $us->isEmpty() ? null : $us->toString();
     }
 }
